de575d7748ca5a59e774c73e4ba13bb29a3f9ffd
NatnaelFY <natfiky2020@gmail.com>
2022-04-07 14:35:45 -0700

    low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - is the first line
  *
- * Return: returns 1 (Success)
+ * Return: returns 1 
  */
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and thet piece of art is
+			useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
