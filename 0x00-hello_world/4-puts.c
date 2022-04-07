ade349d090dc97303356944c99de8b5bfbadcb93
NatnaelFY <natfiky2020@gmail.com>
2022-04-07 12:44:07 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *
- * comment section
- */
-int main(void)
+ *main - the first 
+ *Return: to be follwed
+*/
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
