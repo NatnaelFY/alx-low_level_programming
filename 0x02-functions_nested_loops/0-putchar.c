617c8df33d809eef532cc9a4cdea0af79b1226af
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-04-12 17:51:22 +0300

    low level 3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include "putchar.h"
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: returns 0
+
+ *
+
+ */
+
+int main(void)
+
+{
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+
+}
