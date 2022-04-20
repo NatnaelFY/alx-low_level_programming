2a061c06d7d0aedbcbe10b4952a15667bf104a85
NatnaelFY <natfiky2020@gmail.com>
2022-04-20 22:29:40 +0300

    low level 6

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,8 +11,8 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "XYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCtuvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
