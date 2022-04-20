efa3aea43d1ff54d4edd9dd4de6610b640d47251
NatnaelFY <natfiky2020@gmail.com>
2022-04-20 22:03:24 +0300

    low level 6

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int R;
-	
+
 	i = 0;
 	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 	{
@@ -18,4 +18,5 @@ int _strcmp(char *s1, char *s2)
 	}
 	R = s1[i] - s2[i];
 	return (R);
+
 }
