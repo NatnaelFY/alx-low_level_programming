c757d67fc21f81cc213031c7be079bb910f023ad
NatnaelFY <natfiky2020@gmail.com>
2022-04-20 21:45:20 +0300

    low level 6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * *_strcat - function commute srtings
  * @dest: param pointer to a char
@@ -10,9 +9,9 @@
 char *_strcat(char *dest, char *src)
 
 {
-
 	int i;
 	int j;
+
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
@@ -26,5 +25,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
