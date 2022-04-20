75c04b8dbc50b97d305ec4cb1599a883e847bde5
NatnaelFY <natfiky2020@gmail.com>
2022-04-20 22:49:20 +0300

    low level 6

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rev_string - reverse array
@@ -12,6 +13,7 @@ void rev_string(char *n)
 	int i = 0;
 	int j = 0;
 	char temp;
+	
 	while (*(n + i) != '\0')
 	{
 		i++;
@@ -24,6 +26,7 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -38,6 +41,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
+
 	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
