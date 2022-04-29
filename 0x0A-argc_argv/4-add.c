929998b1b541b1baabe7b55391854ff245c73702
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-04-29 17:01:21 +0300

    low level 10

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,9 +16,9 @@ int check_num(char *str)
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str))/*count string*/
+	while (count < strlen(str))
 	{
-		if (!isdigit(str[count]))/*check if str there are digit*/
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
@@ -38,20 +38,18 @@ int check_num(char *str)
 int main(int argc, char *argv[])
 
 {
-	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
 	count = 1;
-	while (count < argc)/*Goes through the whole array*/
+
+	while (count < argc)
 	{
 		if (check_num(argv[count]))
 		{
-			str_to_int = atoi(argv[count]);/*ATOI --> convert string to int*/
+			str_to_int = atoi(argv[count]);
 			sum += str_to_int;
 		}
-
-		/*Condition If one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
@@ -59,7 +57,7 @@ int main(int argc, char *argv[])
 		}
 		count++;
 	}
-	printf("%d\n", sum);/*print sum*/
+	printf("%d\n", sum);
 
 	return (0);
 }
