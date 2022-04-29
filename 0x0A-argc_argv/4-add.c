9a3fc6be67f5a345b35328ae01dd513a6729b6dc
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-04-29 17:07:06 +0300

    low level 10

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,14 +27,6 @@ int check_num(char *str)
 	return (1);
 }
 
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
- */
-
 int main(int argc, char *argv[])
 
 {
