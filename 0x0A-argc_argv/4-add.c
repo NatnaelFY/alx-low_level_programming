aff630461cfa4d7b5641b60ddded7834e7fe6fa7
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-04-29 17:09:05 +0300

    low level 10

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -26,6 +27,14 @@ int check_num(char *str)
 	}
 	return (1);
 }
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 
