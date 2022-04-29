4bf18600021337edb49ed251d25380b3b765bacb
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-04-29 17:22:53 +0300

    low level 10

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,56 +1,56 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (Success)
  */
-int StringCheck(char *s)
+int check_num(char *str)
 {
-	int i = 0;
+	unsigned int count;
 
-	for (; s[i] != '\0'; i++)
+	count = 0;
+	while (count < strlen(str))
 	{
-		if (!isdigit(s[i]))
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
+		count++;
 	}
 	return (1);
 }
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+	int count;
+	int str_to_int;
 
-	if (argc > 1)
+	int sum = 0;
+	count = 1;
+	while (count < argc)
 	{
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
 		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
-		printf("%d\n", result);
-		return (0);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
+	printf("%d\n", sum);
 
+	return (0);
 }
