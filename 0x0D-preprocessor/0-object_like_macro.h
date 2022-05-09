daac2364ea3378ef794e836213f4df6cf9a17cdd
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-05-09 18:03:06 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,39 +1,6 @@
-#include "main.h"
-/**
- *create_array - array for prints a string
- *@size: number elements array
- *@c: char
- *Return: pointer
- */
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-char *create_array(unsigned int size, char c)
-{
-	char *buffer;
-	unsigned int position;
+#define SIZE 1024
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
-	/*Define values with malloc*/
-	buffer = (char *) malloc(size * sizeof(c));
-
-	if (buffer == 0)
-	{
-		return (NULL);
-	}
-
-	else
-	{
-		position = 0;
-		while (position < size) /*While for array*/
-		{
-			*(buffer + position) = c;
-			position++;
-		}
-
-		return (buffer);
-	}
-
-}
+#endif
