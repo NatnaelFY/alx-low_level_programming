beef10c71c93f9b9adce2b7f8405922cb7f00a0a
NatnaelFY <natfiky2020@gmail.com>
2022-05-11 21:46:20 +0300

    low level 15

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,12 +6,13 @@
  * @name: name to print
  * @f: pointer to the printing function
  */
-void print_name(char *name, void (*f)(char *))
 
+void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 
 		return;
+
 	f(name);
 
 }
