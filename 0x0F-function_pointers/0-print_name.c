6e223e538181f7b2a8aa403bb8c759ca72836e6d
NatnaelFY <natfiky2020@gmail.com>
2022-05-11 22:11:22 +0300

    low level 15

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer to print function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
