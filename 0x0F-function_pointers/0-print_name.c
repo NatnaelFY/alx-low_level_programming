c97054b47d378bcef1a03da0a9d058e41adba10a
NatnaelFY <natfiky2020@gmail.com>
2022-05-11 21:53:31 +0300

    low level 15

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,13 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
+ * @name: name to be printed
+ * @f: callback function that prints name
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-
-    return;
-
-    f(name);
-
+	if (!f || !name)
+		return;
+	(*f)(name);
 }
