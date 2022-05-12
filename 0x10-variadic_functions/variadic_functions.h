77f28a232971c4860e2bfc6c594dca756d8687b8
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-05-12 17:27:07 +0300

    low level 16

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,12 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
-#include <stdlib.h>
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
 
-int _putchar(char);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
