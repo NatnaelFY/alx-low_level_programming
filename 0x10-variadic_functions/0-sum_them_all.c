83a294594e7b230b004c898f20be7bec2690a009
NatnaelFY <102257616+NatnaelFY@users.noreply.github.com>
2022-05-12 17:37:49 +0300

    low level 16

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,34 @@
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
- * Return: the resulting sum
+ * sum_them_all - sum of all its parameters.
+ *@n: num
+ * Return: Sum.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list sum_num;
+	double sum = 0;
 	unsigned int i;
-	int sum = 0;
 
-	va_list list;
+	/* initialize valist for num number of arguments */
+	va_start(sum_num, n);
 
-	va_start(list, n);
+	if (n == 0)
+	{
+		return (0);
+	}
 
-	for (i = 0; i < n; i++)
+	/* access all the arguments assigned to valist */
+	i = 0;
+	while (i < n)
+	{
+		sum += va_arg(sum_num, int);
+		i++;
+	}
 
-		sum += va_arg(list, int);
-
-	va_end(list);
+	/* clean memory reserved for valist */
+	va_end(sum_num);
 
 	return (sum);
 }
