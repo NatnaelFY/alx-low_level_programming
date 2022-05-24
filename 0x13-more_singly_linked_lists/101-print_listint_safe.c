54d3694cbb9690954f6129a732a213a1163893a9
NatnaelFY <natfiky2020@gmail.com>
2022-05-24 22:44:54 +0300

    low level 18

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
- *
- * Return: number of nodes in the list
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to head of list
+ * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
