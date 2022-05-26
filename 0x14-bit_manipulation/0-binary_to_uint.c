1bb0e0896a9d239debf38dbd9dd356189b6f7f53
NatnaelFY <natfiky2020@gmail.com>
2022-05-27 02:58:14 +0300

    low level 19

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-/**
- * binary_to_uint  - converts a binary number to an unsigned int
- * @b:  pointing to a string of 0 and 1 chars
- * Return:   converted number or 0 if b is NULL
- */
 unsigned int binary_to_uint(const char *b)
 {
 
