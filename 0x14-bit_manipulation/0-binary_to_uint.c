7765cc917d55b553942e92f83b7a33ad65b3837f
NatnaelFY <natfiky2020@gmail.com>
2022-05-27 02:57:03 +0300

    ow level 19

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i;
 
-	/* if string is empty return 0 */
 	if (b == NULL)
 		return (0);
 
-	/* return if the value is different to 0 or 1*/
 	for (i = 0; b[i] != '\0'; i++)
 		if (b[i]  != '0' && b[i] != '1')
 			return (0);
 
-	/* converter array the number binary to  int */
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		num *= 2;
@@ -28,7 +25,5 @@ unsigned int binary_to_uint(const char *b)
 			num += 1;
 	}
 	return (num);
-
-
 
 }
